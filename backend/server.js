508cf08399e4a017422d1613897bf9813e17abb6
Priyansh <105690577+Priyanshrai@users.noreply.github.com>
2025-01-20 02:21:06 +0530

    updated vercel url

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-vercel-app-url.vercel.app', 'https://your-custom-domain.com'] 
+    ? ['https://ecommerce-project-gex2yqi2o-priyanshrais-projects.vercel.app', 'https://ecommerce-project-ochre-six.vercel.app'] 
     : 'http://localhost:3000'
 }));
 app.use(express.json());
